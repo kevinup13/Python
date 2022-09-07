3656f3ec227d25e53dfb7ab6709f36d145ac1b68
kevin lopes albuquerque <kevinup13@gmail.com>
2022-09-07 15:15:00 -0400

    Gerador de jogos lotofacil

--- Operadores/lotofacil.py
+++ Operadores/lotofacil.py
@@ -1,24 +1,30 @@
 # Entrada de numero de jogos
 # Cria uma lista com 15 numeros aleatórios
-
 import random
+import time
 
 print("======"*10)
 print("==="*8, " LOTOFÁCIL ", "==="*8)
 print("======"*10)
 
 jogo = 0
+""" Insere a quantidade de jogos  """
 numero_jogos = input("\n>> Digite o número de jogos: ")
 print()
-for l in range(int(numero_jogos)):
+for linha in range(int(numero_jogos)):
     jogo += 1
-    numeros = random.sample(range(1,25),15)
-    for c in range(1):
-        numeros = random.sample(range(1,25),15)
+    numeros = random.sample(range(1, 25), 15)
+    for coluna in range(1):
+        numeros = random.sample(range(1, 25), 15)
         print(f'>> Jogo {jogo}: ')
-        print(numeros, end='')    
+        print(numeros, end='')
     print()
-    print("---"*20)   
-print("==="*8,"Boa sorte!","==="*8)
+    print("---"*20)
+    time.sleep(1)
 
-
+for n in range(1):
+    print("==="*8, end='')
+    for num in " Boa Sorte! ":
+        print(num, flush=True, end='')
+        time.sleep(0.3)
+    print("==="*8)
